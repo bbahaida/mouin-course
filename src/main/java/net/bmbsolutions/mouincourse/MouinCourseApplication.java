c9321659fcc98c8e323bb097aad273c7b67a1eaf
Mohamed El-Alem <mouinalem@gmail.com>
2025-01-22 21:31:42 +0000

    ci: fix build

--- src/main/java/net/bmbsolutions/mouincourse/MouinCourseApplication.java
+++ src/main/java/net/bmbsolutions/mouincourse/MouinCourseApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MouinCourseApplication {
 
     public static void main(String[] args) {
-        xSpringApplication.run(MouinCourseApplication.class, args);
+        SpringApplication.run(MouinCourseApplication.class, args);
     }
 
 }
